3b6d78b0cafc1738789871b74fa90ad160458e1b
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-09-05 19:49:01 -0400

    complete Lucians Luscious Lasagna

--- fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -1,9 +1,15 @@
 module LuciansLusciousLasagna
 
 // TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
 
 // TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven timeInOven = expectedMinutesInOven - timeInOven
 
 // TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes numberOfLayers = numberOfLayers * 2
 
 // TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes numberOfLayers minutesInOven =
+    preparationTimeInMinutes numberOfLayers
+    + minutesInOven
