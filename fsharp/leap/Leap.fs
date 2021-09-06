4ffd0135b24aa09c7482510e08d024fdf1c95082
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-09-06 17:07:26 -0400

    implement leap year

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,3 +1,10 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = failwith "You need to implement this function."
+// a leap year in the Gregorian calendar occurs:
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+
+let leapYear year =
+    year % 4 = 0
+    && (year % 100 <> 0 || year % 400 = 0)
