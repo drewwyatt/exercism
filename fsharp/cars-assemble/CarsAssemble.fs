b1fbbf052a63bda056b9d5064054e71df8f7e680
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-09-05 20:52:40 -0400

    task 1

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,7 +1,27 @@
 module CarsAssemble
 
-let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+// At its lowest speed (`1`), `221` cars are produced each hour.
+// The production increases linearly with the speed. So with the speed set to `4`, it should produce `4 * 221 = 884` cars per hour.
+// However, higher speeds increase the likelihood that faulty cars are produced, which then have to be discarded. The following table shows how speed influences the success rate:
 
-let workingItemsPerMinute(speed: int): int =
-   failwith "Please implement the 'workingItemsPerMinute' function"
+// - `0`: 0% success rate.
+// - `1` to `4`: 100% success rate.
+// - `5` to `8`: 90% success rate.
+// - `9`: 80% success rate.
+// - `10`: 77% success rate.
+
+
+let productionRatePerHour (speed: int) : float =
+    let successRate =
+        if speed < 5 then 1.0
+        else if speed < 9 then 0.9
+        else if speed = 9 then 0.8
+        else 0.77
+
+    (float (speed * 221)) * successRate
+
+
+
+
+let workingItemsPerMinute (speed: int) : int =
+    failwith "Please implement the 'workingItemsPerMinute' function"
