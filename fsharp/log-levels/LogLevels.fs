07000f6d1621f3c44b198831295608aa305b18b0
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-09-07 23:28:47 -0400

    message + logLevel

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,7 +1,43 @@
 module LogLevels
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
+let splitLog (line: string) =
+    let pieces = line.Split ":"
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+    if Array.length pieces = 2 then
+        Some(pieces.[0].Trim(), pieces.[1].Trim())
+    else
+        None
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let (|INFO|_|) input =
+    match splitLog input with
+    | Some ("[INFO]", message) -> Some(message)
+    | _ -> None
+
+let (|WARNING|_|) input =
+    match splitLog input with
+    | Some ("[WARNING]", message) -> Some(message)
+    | _ -> None
+
+let (|ERROR|_|) input =
+    match splitLog input with
+    | Some ("[ERROR]", message) -> Some(message)
+    | _ -> None
+
+
+let message logLine =
+    match logLine with
+    | INFO message
+    | WARNING message
+    | ERROR message -> message
+    | _ -> failwith (sprintf "could not parse log line '%s'" logLine)
+
+
+let logLevel logLine =
+    match logLine with
+    | INFO _ -> "info"
+    | WARNING _ -> "warning"
+    | ERROR _ -> "error"
+    | _ -> failwith (sprintf "could not parse log line '%s'" logLine)
+
+let reformat (logLine: string) : string =
+    failwith "Please implement the 'reformat' function"
